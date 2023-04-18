fbe9655144870f169e38d35b7a72dfbf9a14a358
Sam <samismos@yahoo.gr>
2023-04-18 21:08:37 +0300

    Testing static wp-page for gh pages

---
+++ wp-login.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en-US">
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<title>Log In &lsaquo; GFSA &#8212; WordPress</title>
+	<meta name="robots" content="max-image-preview:large, noindex, noarchive">
+<link rel="stylesheet" id="dashicons-css" href="http://samismos.github.io/GFSA_WP_static/wp-includes/css/dashicons.min.css?ver=6.2" type="text/css" media="all">
+<link rel="stylesheet" id="buttons-css" href="http://samismos.github.io/GFSA_WP_static/wp-includes/css/buttons.min.css?ver=6.2" type="text/css" media="all">
+<link rel="stylesheet" id="forms-css" href="http://samismos.github.io/GFSA_WP_static/wp-admin/css/forms.min.css?ver=6.2" type="text/css" media="all">
+<link rel="stylesheet" id="l10n-css" href="http://samismos.github.io/GFSA_WP_static/wp-admin/css/l10n.min.css?ver=6.2" type="text/css" media="all">
+<link rel="stylesheet" id="login-css" href="http://samismos.github.io/GFSA_WP_static/wp-admin/css/login.min.css?ver=6.2" type="text/css" media="all">
+	<meta name="referrer" content="strict-origin-when-cross-origin">
+		<meta name="viewport" content="width=device-width">
+		</head>
+	<body class="login no-js login-action-login wp-core-ui  locale-en-us">
+	<script type="text/javascript">document.body.className = document.body.className.replace('no-js','js');</script>
+		<div id="login">
+		<h1><a href="https://wordpress.org/">Powered by WordPress</a></h1>
+	
+		<form name="loginform" id="loginform" action="http://localhost/wordpress/wp-login.php" method="post">
+			<p>
+				<label for="user_login">Username or Email Address</label>
+				<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off" autocomplete="username">
+			</p>
+
+			<div class="user-pass-wrap">
+				<label for="user_pass">Password</label>
+				<div class="wp-pwd">
+					<input type="password" name="pwd" id="user_pass" class="input password-input" value="" size="20" autocomplete="current-password" spellcheck="false">
+					<button type="button" class="button button-secondary wp-hide-pw hide-if-no-js" data-toggle="0" aria-label="Show password">
+						<span class="dashicons dashicons-visibility" aria-hidden="true"></span>
+					</button>
+				</div>
+			</div>
+						<p class="forgetmenot"><input name="rememberme" type="checkbox" id="rememberme" value="forever"> <label for="rememberme">Remember Me</label></p>
+			<p class="submit">
+				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In">
+									<input type="hidden" name="redirect_to" value="http://localhost/wordpress/wp-admin/">
+									<input type="hidden" name="testcookie" value="1">
+			</p>
+		</form>
+
+					<p id="nav">
+				<a href="http://samismos.github.io/GFSA_WP_static/wp-login.php?action=lostpassword">Lost your password?</a>			</p>
+					<script type="text/javascript">function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_login" );d.focus(); d.select();} catch( er ) {}}, 200);}
+wp_attempt_focus();
+if ( typeof wpOnload === 'function' ) { wpOnload() }</script>
+				<p id="backtoblog">
+			<a href="http://samismos.github.io/GFSA_WP_static/">&larr; Go to GFSA</a>		</p>
+			</div>
+			<script type="text/javascript" id="essential-blocks-blocks-localize-js-extra">/*  */
+var EssentialBlocksLocalize = {"eb_plugins_url":"http:\/\/samismos.github.io\/GFSA_WP_static\/wp-content\/plugins\/essential-blocks\/","eb_wp_version":"6.2","eb_version":"4.0.8","eb_admin_url":"http:\/\/samismos.github.io\/GFSA_WP_static\/wp-admin\/","rest_rootURL":"http:\/\/samismos.github.io\/GFSA_WP_static\/wp-json\/","ajax_url":"http:\/\/samismos.github.io\/GFSA_WP_static\/wp-admin\/admin-ajax.php","nft_nonce":"29bbe150da"};
+/*  */</script>
+<script type="text/javascript" src="http://samismos.github.io/GFSA_WP_static/wp-content/plugins/essential-blocks/assets/js/eb-blocks-localize.js?ver=4.0.8" id="essential-blocks-blocks-localize-js"></script>
+<script type="text/javascript" src="http://samismos.github.io/GFSA_WP_static/wp-includes/js/jquery/jquery.min.js?ver=3.6.3" id="jquery-core-js"></script>
+<script type="text/javascript" src="http://samismos.github.io/GFSA_WP_static/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.4.0" id="jquery-migrate-js"></script>
+<script type="text/javascript" id="zxcvbn-async-js-extra">/*  */
+var _zxcvbnSettings = {"src":"http:\/\/samismos.github.io\/GFSA_WP_static\/wp-includes\/js\/zxcvbn.min.js"};
+/*  */</script>
+<script type="text/javascript" src="http://samismos.github.io/GFSA_WP_static/wp-includes/js/zxcvbn-async.min.js?ver=1.0" id="zxcvbn-async-js"></script>
+<script type="text/javascript" src="http://samismos.github.io/GFSA_WP_static/wp-includes/js/dist/vendor/wp-polyfill-inert.min.js?ver=3.1.2" id="wp-polyfill-inert-js"></script>
+<script type="text/javascript" src="http://samismos.github.io/GFSA_WP_static/wp-includes/js/dist/vendor/regenerator-runtime.min.js?ver=0.13.11" id="regenerator-runtime-js"></script>
+<script type="text/javascript" src="http://samismos.github.io/GFSA_WP_static/wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=3.15.0" id="wp-polyfill-js"></script>
+<script type="text/javascript" src="http://samismos.github.io/GFSA_WP_static/wp-includes/js/dist/hooks.min.js?ver=4169d3cf8e8d95a3d6d5" id="wp-hooks-js"></script>
+<script type="text/javascript" src="http://samismos.github.io/GFSA_WP_static/wp-includes/js/dist/i18n.min.js?ver=9e794f35a71bb98672ae" id="wp-i18n-js"></script>
+<script type="text/javascript" id="wp-i18n-js-after">
+wp.i18n.setLocaleData( { 'text directionltr': [ 'ltr' ] } );
+</script>
+<script type="text/javascript" id="password-strength-meter-js-extra">/*  */
+var pwsL10n = {"unknown":"Password strength unknown","short":"Very weak","bad":"Weak","good":"Medium","strong":"Strong","mismatch":"Mismatch"};
+/*  */</script>
+<script type="text/javascript" src="http://samismos.github.io/GFSA_WP_static/wp-admin/js/password-strength-meter.min.js?ver=6.2" id="password-strength-meter-js"></script>
+<script type="text/javascript" src="http://samismos.github.io/GFSA_WP_static/wp-includes/js/underscore.min.js?ver=1.13.4" id="underscore-js"></script>
+<script type="text/javascript" id="wp-util-js-extra">/*  */
+var _wpUtilSettings = {"ajax":{"url":"\/wordpress\/wp-admin\/admin-ajax.php"}};
+/*  */</script>
+<script type="text/javascript" src="http://samismos.github.io/GFSA_WP_static/wp-includes/js/wp-util.min.js?ver=6.2" id="wp-util-js"></script>
+<script type="text/javascript" id="user-profile-js-extra">/*  */
+var userProfileL10n = {"user_id":"0","nonce":"3ea8cd8c58"};
+/*  */</script>
+<script type="text/javascript" src="http://samismos.github.io/GFSA_WP_static/wp-admin/js/user-profile.min.js?ver=6.2" id="user-profile-js"></script>
+	<div class="clear"></div>
+	</body>
+	</html>
